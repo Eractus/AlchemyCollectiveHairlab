14830e135ff1f64536558512b8154df2c59a50a7
Danny Peng <dannyw.peng@gmail.com>
2018-11-28 15:51:04 -0800

    finish splash page styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -51,7 +51,13 @@ class Splash extends React.Component {
           <button>Coming Soon</button>
         </section>
         <section className="splash-reviews">
-          <p>Here's What Our Customers Have to Say</p>
+          <div className="splash-reviews-text">
+            <h3>Here's What Our Customers Have to Say</h3>
+            <h5>"Kefka is the man! Five stars!!!"</h5>
+            <p>-Ameer A.</p>
+            <h5>"The ACH team has winning members. Highly recommended!"</h5>
+            <p>-Jon W.</p>
+          </div>
           <Link className="splash-button" to="/reviews"><button>All Reviews</button></Link>
         </section>
       </main>
