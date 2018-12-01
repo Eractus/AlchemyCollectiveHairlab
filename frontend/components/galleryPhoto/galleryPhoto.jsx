279ac6e14b95d00c383504c9562a2ed9407ac01e
Danny Peng <dannyw.peng@gmail.com>
2018-11-30 18:32:09 -0800

    added click and popup photo feature for gallery with syling

--- frontend/components/galleryPhoto/galleryPhoto.jsx
+++ frontend/components/galleryPhoto/galleryPhoto.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const GalleryPhoto = props => {
-  console.log(props.photo);
-  return (
-    <li className="gallery-photo-item-container">
-      <div>
-        <Link to={`/`}>
-          <img className="gallery-photo-image" src={props.photo} />
-        </Link>
+class GalleryPhoto extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { displayPhoto: false }
+
+    this.openPhotoPopup = this.openPhotoPopup.bind(this);
+    this.closePhotoPopup = this.closePhotoPopup.bind(this);
+  }
+
+  openPhotoPopup() {
+    this.setState({ displayPhoto: true });
+  }
+
+  closePhotoPopup() {
+    this.setState({ displayPhoto: false });
+  }
+
+  render() {
+    const photoPopup = (this.state.displayPhoto) ?
+    <div className="modal-overlay">
+      <div className="photo-image-popup">
+        <img src={this.props.photo} />
+        <span onClick={this.closePhotoPopup}></span>
       </div>
-    </li>
-  );
-};
+    </div> : "";
+
+    return (
+      <li className="gallery-photo-item-container">
+        {photoPopup}
+        <div>
+          <span>
+            <img onClick={this.openPhotoPopup} className="gallery-photo-image" src={this.props.photo} />
+          </span>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default GalleryPhoto;
